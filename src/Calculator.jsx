a2af19fd94434fb29af75a1d7ac39fba728c78c1
Danilo Melo <danilomarquesdemelo@gmail.com>
2023-03-10 15:20:20 -0300

    Implemented the calculation logic

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,42 +1,70 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Calculator.css'
 import Display from './components/Display'
 import Button from './components/Button'
 
 export default props => {
+    const [displayValue, setDisplayValue] = useState(0)
+    const [saveNum, setSaveNum] = useState(0)
+    const [operator, setOperator] = useState(0)
 
     function clearDisplay(){
-        console.log('Cleanning...')
+        setDisplayValue(0)
     }
 
-    function getOperation(opr){
+    function getDigit(d){
+        displayValue === 0 ? setDisplayValue(d) : setDisplayValue(displayValue + d)  
+    }
+
+    function percentage (){
+        setDisplayValue( displayValue / 100)
+    }
+
+    function switchPositiveNegative(){
+        displayValue > 0 ? setDisplayValue(-displayValue) : setDisplayValue(Math.abs(displayValue))
+    }
+
+    function operatorHandler(opr){
+        setOperator(opr)
+        setSaveNum(displayValue)
+        setDisplayValue(0)
         console.log(opr)
     }
 
-    function getDigit(digit){
-        console.log(digit)
+    function calc(){
+        if(operator === '+'){
+            setDisplayValue(parseFloat(saveNum) + parseFloat(displayValue))
+        }else if(operator === '-'){
+            setDisplayValue(saveNum - displayValue)
+        }else if(operator === '*'){
+            setDisplayValue(saveNum * displayValue)
+        }else if(operator === '/'){
+            setDisplayValue(saveNum / displayValue)
+        }
     }
 
     return (
         <div className='calculator'>
-            <Display value={100} />
-            <Button label='AC' click={clearDisplay} triple />
-            <Button label='/' click={getOperation} operation/>
-            <Button label='7' click={getDigit} />
-            <Button label='8' click={getDigit} />
-            <Button label='9' click={getDigit} />
-            <Button label='*' click={getOperation} operation/>
-            <Button label='4' click={getDigit} />
-            <Button label='5' click={getDigit} />
-            <Button label='6' click={getDigit} />
-            <Button label='-' click={getOperation} operation/>
-            <Button label='1' click={getDigit} />
-            <Button label='2' click={getDigit} />
-            <Button label='3' click={getDigit} />
-            <Button label='+' click={getOperation} operation/>
-            <Button label='0' click={getDigit} double/>
-            <Button label='.' click={getDigit} />
-            <Button label='=' click={getOperation} operation/>
+            <Display value={displayValue} />
+            <Button label='AC' click={clearDisplay}/>
+            <Button label='+/-'click={switchPositiveNegative} />
+            <Button label='%' click={percentage}/>
+            <Button label='/' click={operatorHandler}operation />
+            <Button label='7' click={getDigit}/>
+            <Button label='8' click={getDigit}/>
+            <Button label='9' click={getDigit}/>
+            <Button label='*' click={operatorHandler}operation />
+            <Button label='4' click={getDigit}/>
+            <Button label='5' click={getDigit}/>
+            <Button label='6' click={getDigit}/>
+            <Button label='-' click={operatorHandler}operation />
+            <Button label='1' click={getDigit}/>
+            <Button label='2' click={getDigit}/>
+            <Button label='3' click={getDigit}/>
+            <Button label='+' click={operatorHandler}operation />
+            <Button label='0' click={getDigit}double/>
+            <Button label='.' click={getDigit}/>
+            <Button label='=' click={calc} operation />
         </div>
     )
 }
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,8 +5,23 @@ export default props => {
 
     let cssClasses = 'button '
     cssClasses += props.double ? 'double ' : ''
-    cssClasses += props.triple ? 'triple ' : ''
     cssClasses += props.operation ? 'operation ' : ''
+
+    // function operations(props){
+    //     if(props.label === '+'){
+    //         console.log('cliclou na soma')
+    //     }else if(props.label === '-'){
+    //         console.log('clicou na subtracao')
+    //     }else if(props.label === '*'){
+    //         console.log('clicou na multiplicacao')
+    //     }else if(props.label === '/'){
+    //         console.log('clicou na divisao')
+    //     }else{
+    //         return props.label
+    //     }
+    // }
+
+
 
     return (
         <button className={cssClasses} onClick={e => props.click(props.label)}>
